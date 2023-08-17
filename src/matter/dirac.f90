821357a7acedefe9063bbf75950100743f7bbc1e
malcubi <malcubi@nucleares.unam.mx>
2023-08-17 14:24:41 -0600

    Sources for dirac field. Still an empty shell.

---
+++ src/matter/dirac.f90
@@ -0,0 +1,27 @@
+!$Header: /usr/local/ollincvs/Codes/OllinSphere-BiB/src/matter/dirac.f90,v 1.1 2023/08/17 20:18:37 malcubi Exp $
+
+  subroutine sources_dirac(l)
+
+! Include modules.
+
+  use procinfo
+  use param
+  use arrays
+
+! Extra variables.
+
+  implicit none
+
+  integer l
+
+
+! ***********************************
+! ***   SOURCES FOR DIRAC FIELD   ***
+! ***********************************
+
+
+! ***************
+! ***   END   ***
+! ***************
+
+  end subroutine sources_dirac
