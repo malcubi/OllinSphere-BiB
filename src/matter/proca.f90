4198e6e20064dcec8d668f79d6b69e7d117d5c8b
malcubi <malcubi@nucleares.unam.mx>
2025-11-18 13:18:40 -0600

    Improving boundaries

--- src/matter/proca.f90
+++ src/matter/proca.f90
@@ -1,4 +1,4 @@
-!$Header: /usr/local/ollincvs/Codes/OllinSphere-BiB/src/matter/proca.f90,v 1.21 2023/02/22 21:29:19 malcubi Exp $
+!$Header: /usr/local/ollincvs/Codes/OllinSphere-BiB/src/matter/proca.f90,v 1.22 2025/11/18 19:58:12 malcubi Exp $
 
   subroutine sources_proca(l)
 
@@ -151,51 +151,24 @@
 ! ***   BOUNDARIES   ***
 ! **********************
 
-! The electric field evolves only through sources (since there
-! is no magnetic field), so it needs no boundary condition.
+! The proca electric field evolves only through sources (since
+! there is no magnetic field), so it needs no boundary condition.
 ! For the potentials we do need a boundary condition.
 
-! Boundary condition for procaPhi: We assume a simple outgoing
-! spherical wave.  We also add a contribution to the source (-E/2).
-! This term comes from expressing ePhi in terms of incoming and
-! outgoing eigenfields, and then assuming the incoming eigenfield
-! is very small.
+! For procaPhi and procaA I have experimented with radiative boundary
+! conditions for both, assuming that far away they behave as f(r-t)/r.
+! It turns out that the best well behaved case is to aply the radiative
+! condition only to procaA, and leave procaPhi to evolve freely all the
+! way to the boundary. This seems to be stable and the Gauss constraint
+! converges to zero.
 
-  sprocaPhi(l,Nr) = - (D1_procaPhi(l,Nr) + procaPhi(l,Nr)/r(l,Nr)) - 0.5d0*procaE(l,Nr)
+! Boundary condition for procaPhi.
 
-! The variable procaA in fact does not need a boundary condition,
-! but ... it turns out that for this type of first order system
-! the higher order one-sided derivatives cause instabilities,
-! so here I recalculate the sources close to the boundary to
-! fourth order.
+  !sprocaPhi(l,Nr) = - (D1_procaPhi(l,Nr) + procaPhi(l,Nr)/r(l,Nr))
 
-  do i=Nr-ghost+2,Nr-1
+! Boundary condition for procA.
 
-     aux = (3.d0*procaH(l,i+1) + 10.d0*procaH(l,i) - 18.d0*procaH(l,i-1) &
-         + 6.d0*procaH(l,i-2) - procaH(l,i-3))/12.d0/dr(l)
-
-     sprocaPhi(l,i) = one/(A(l,i)*psi4(l,i))*(procaH(l,i)*(half*D1_A(l,i)/A(l,i) - D1_B(l,i)/B(l,i) &
-                    - 2.d0*D1_phi(l,i) - 2.d0/r(l,i)) - aux) + trK(l,i)*procaF(l,i)
-
-     aux = (3.d0*procaF(l,i+1) + 10.d0*procaF(l,i) - 18.d0*procaF(l,i-1) &
-         + 6.d0*procaF(l,i-2) - procaF(l,i-3))/12.d0/dr(l)
-
-     sprocaA(l,i) = - alpha(l,i)*A(l,i)*psi4(l,i)*procaE(l,i) - aux
-
-     if (shift/="none") then
-        sprocaPhi(l,i) = sprocaPhi(l,i) + beta(l,i)*DA_procaPhi(l,i)
-        sprocaA(l,i) = sprocaA(l,i) + beta(l,i)*DA_procaA(l,i) + procaA(l,i)*D1_beta(l,i)
-     end if
-
-  end do
-
-! Source for procaA at last point.
-
-  sprocaA(l,Nr) = - alpha(l,Nr)*A(l,Nr)*psi4(l,Nr)*procaE(l,Nr) - D1_procaF(l,Nr)
-
-  if (shift/="none") then
-     sprocaA(l,Nr) = sprocaA(l,Nr) + beta(l,Nr)*DA_procaA(l,Nr) + procaA(l,Nr)*D1_beta(l,Nr)
-  end if
+  sprocaA(l,Nr) = - (D1_procaA(l,Nr) + procaA(l,Nr)/r(l,Nr))
 
 
 ! ***************
