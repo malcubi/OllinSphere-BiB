eaf1a15beb0f891e860d2bc2c4eed1fcd665b14f
malcubi <malcubi@nucleares.unam.mx>
2026-01-08 17:46:49 -0600

    minor change

--- src/matter/idata_ChargedDiracstar.f90
+++ src/matter/idata_ChargedDiracstar.f90
@@ -744,8 +744,8 @@
      omega_new = dirac_omega/alphafac
 
      if (rank==0) then
-        write(*,'(A,E22.16)') ' Omega (not-rescaled)      = ', dirac_omega
-        write(*,'(A,E22.16)') ' Omega (rescaled)          = ', omega_new
+        write(*,'(A,E23.16)') ' Omega (not-rescaled)      = ', dirac_omega
+        write(*,'(A,E23.16)') ' Omega (rescaled)          = ', omega_new
      end if
 
 !    Rescale maxwellF.
