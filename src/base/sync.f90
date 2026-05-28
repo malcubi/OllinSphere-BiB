290e0dd8a8ea9e0a2c31169b3a2004919d5495fa
malcubi <malcubi@nucleares.unam.mx>
2026-05-28 14:35:10 -0600

    cleaning up

--- src/base/sync.f90
+++ src/base/sync.f90
@@ -1,4 +1,3 @@
-!$Header: /usr/local/ollincvs/Codes/OllinSphere-BiB/src/base/sync.f90,v 1.6 2019/05/22 18:11:09 malcubi Exp $
 
   subroutine sync
 
@@ -18,7 +17,6 @@
   implicit none
 
   integer i,p
-  integer i0
   integer status(MPI_STATUS_SIZE)
 
   real(8), dimension(1:ghost) :: w     ! Auxiliary array for efficient communications.
@@ -28,64 +26,48 @@
 ! ***   SYNCHRONIZATION   ***
 ! ***************************
 
-! Since we are using a pointer "syncvar" we have a problem with
-! the lower and upper bounds.  If you have a pointer with
-! identical dimensions to an array it copies its bounds, but
-! if the pointer has lower dimensions the lower bound is
-! always set to 1.  So here we need to find out the real
-! lower bound of the pointer array and compensate.
- 
-  i0 = lbound(syncvar,dim=1) - 1 + ghost
+! Send information to processor on the left
+! (only if we are not on axis).
 
-! We only need to synchronize if we have
-! more than one processor.
+  if (rank/=0) then
+     p = rank-1
+     do i=1,ghost
+        w(i) = syncvar(i)
+     end do
+     call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
+  end if
 
-  if (size>1) then
+! Send information to processor on the right
+! (only if we are not at outer boundary).
 
-!    Send information to processor on the left
-!    (only if we are not on axis).
+  if (rank<size-1) then
+     p = rank+1
+     do i=1,ghost
+        w(i) = syncvar(Nr-2*ghost+i)
+     end do
+     call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
+  end if
 
-     if (rank/=0) then
-        p = rank-1
-        do i=1,ghost
-           w(i) = syncvar(i+i0)
-        end do
-        call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
-     end if
+! Receive information from processor on the right
+! (only if we are not at outer boundary).
 
-!    Send information to processor on the right
-!    (only if we are not at outer boundary).
+  if (rank<size-1) then
+     p = rank+1
+     call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
+     do i=1,ghost
+        syncvar(Nr-ghost+i) = w(i)
+     end do
+  end if
 
-     if (rank<size-1) then
-        p = rank+1
-        do i=1,ghost
-           w(i) = syncvar(Nr-2*ghost+i+i0)
-        end do
-        call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
-     end if
+! Receive information from processor on the left
+! (only if we are not on axis).
 
-!    Receive information from processor on the right
-!    (only if we are not at outer boundary).
-
-     if (rank<size-1) then
-        p = rank+1
-        call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
-        do i=1,ghost
-           syncvar(Nr-ghost+i+i0) = w(i)
-        end do
-     end if
-
-!    Receive information from processor on the left
-!    (only if we are not on axis).
-
-     if (rank/=0) then
-        p = rank-1
-        call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
-        do i=1,ghost
-           syncvar(i-ghost+i0) = w(i)
-        end do
-     end if
-
+  if (rank/=0) then
+     p = rank-1
+     call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
+     do i=1,ghost
+        syncvar(i-ghost) = w(i)
+     end do
   end if
 
 
