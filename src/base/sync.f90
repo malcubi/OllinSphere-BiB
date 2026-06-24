e9508c78238a3de972dc45561d47ecb2b4491c49
malcubi <malcubi@nucleares.unam.mx>
2026-06-24 12:49:49 -0600

    optimizing

--- src/base/sync.f90
+++ src/base/sync.f90
@@ -16,58 +16,49 @@
 
   implicit none
 
-  integer i,p
-  integer status(MPI_STATUS_SIZE)
+  integer :: left,right
+  integer :: status(MPI_STATUS_SIZE)
 
-  real(8), dimension(1:ghost) :: w     ! Auxiliary array for efficient communications.
+  real(8) :: wsend(ghost),wrecv(ghost)
+
+  left  = rank-1
+  right = rank+1
 
 
-! ***************************
-! ***   SYNCHRONIZATION   ***
-! ***************************
+! *************************************
+! ***   Exchange left ghost zones   ***
+! *************************************
 
-! Send information to processor on the left
-! (only if we are not on axis).
 
-  if (rank/=0) then
-     p = rank-1
-     do i=1,ghost
-        w(i) = syncvar(i)
-     end do
-     call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
+  if (left >= 0) then
+
+     wsend = syncvar(1:ghost)
+
+     call MPI_Sendrecv( &
+        wsend, ghost, MPI_REAL8, left, 1, &
+        wrecv, ghost, MPI_REAL8, left, 2, &
+        MPI_COMM_WORLD, status, ierr)
+
+     syncvar(1-ghost:0) = wrecv
+
   end if
 
-! Send information to processor on the right
-! (only if we are not at outer boundary).
 
-  if (rank<size-1) then
-     p = rank+1
-     do i=1,ghost
-        w(i) = syncvar(Nr-2*ghost+i)
-     end do
-     call MPI_SEND(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,ierr)
-  end if
+! **************************************
+! ***   Exchange right ghost zones   ***
+! **************************************
 
-! Receive information from processor on the right
-! (only if we are not at outer boundary).
+  if (right < size) then
 
-  if (rank<size-1) then
-     p = rank+1
-     call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
-     do i=1,ghost
-        syncvar(Nr-ghost+i) = w(i)
-     end do
-  end if
+     wsend = syncvar(Nr-2*ghost+1:Nr-ghost)
 
-! Receive information from processor on the left
-! (only if we are not on axis).
+     call MPI_Sendrecv( &
+        wsend, ghost, MPI_REAL8, right, 2, &
+        wrecv, ghost, MPI_REAL8, right, 1, &
+        MPI_COMM_WORLD, status, ierr)
 
-  if (rank/=0) then
-     p = rank-1
-     call MPI_RECV(w,ghost,MPI_REAL8,p,1,MPI_COMM_WORLD,status,ierr)
-     do i=1,ghost
-        syncvar(i-ghost) = w(i)
-     end do
+     syncvar(Nr-ghost+1:Nr) = wrecv
+
   end if
 
 
